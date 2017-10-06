cb128e520c19a29855f5e137e7eec7ef69fc82e9
Johnfred Cornell <johnfred.cornell.287@my.csun.edu>
2017-10-06 12:30:20 -0700

    Gets video feed from Gazebo (via local server). First attempt at implementation.

---
+++ NetworkingTest/Assets/Scripts/GrabText.cs
@@ -0,0 +1,45 @@
+﻿//This script was desinged to grab some text from a local server.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.IO;
+using UnityEngine.UI;
+
+public class GrabText : MonoBehaviour {
+
+    public Text webText;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(GetText());
+    }
+    //Handles GettingText
+    IEnumerator GetText()
+    {
+        //comment switch depending on server
+        //UnityWebRequest www = UnityWebRequest.Get("localhost:3000");
+        //UnityWebRequest www = UnityWebRequest.Get("192.168.1.161:9090");
+        //Gazeebo Text
+        //UnityWebRequest www = UnityWebRequest.Get("http://192.168.1.161:8080");
+        //JS Server
+        UnityWebRequest www = UnityWebRequest.Get("http://192.168.1.142:8080/upload");
+
+        yield return www.Send();
+
+        if (www.isNetworkError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
+    }
+}
---
+++ NetworkingTest/Assets/Scripts/GrabTexture.cs
@@ -0,0 +1,50 @@
+﻿//This script is designed to get the video feed from a local Web_Video_Server and play it back in Unity.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.IO;
+using UnityEngine.UI;
+
+public class GrabTexture : MonoBehaviour
+{
+    Texture myTexture;
+    //public Text webText;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(GetTexture());
+    }
+
+
+    //Handles Getting Texture.
+    IEnumerator GetTexture()
+    {
+        //Video feed from Gazeebo
+        //UnityWebRequest www = UnityWebRequestTexture.GetTexture("http://192.168.1.161:8080/stream_viewer?topic=/stereo/left/image_raw");
+        // UnityWebRequest www = UnityWebRequestTexture.GetTexture("http://192.168.1.161:8080/stream_viewer?topic=/stereo/right/image_raw");
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture("http://192.168.1.161:8080/stream_viewer?topic=/stereo/left/image_raw");
+        yield return www.Send();
+
+        if (www.isNetworkError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+        }
+    }
+    //On GUI handles texture output (Display to Screen).
+    void OnGUI()
+    {
+        if (!myTexture)
+        {
+            Debug.LogError("Assign a Texture in the inspector.");
+            return;
+        }
+        GUI.DrawTexture(new Rect(0, 0, 400, 300), myTexture, ScaleMode.StretchToFill, true, 10.0F);
+    }
+}
